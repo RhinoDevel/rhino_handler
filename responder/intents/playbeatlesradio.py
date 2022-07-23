8ad356fe1b52e7a42584084f1649d2a67c9910e6
Marc <RhinoDevel@users.noreply.github.com>
2022-07-23 22:23:20 +0200

    Play Beatles radio intent.

---
+++ responder/intents/playbeatlesradio.py
@@ -0,0 +1,10 @@
+
+# Marcel Timm, RhinoDevel, 2022jul23
+
+from audio_player import audio_player
+
+LOCATION = 'https://strw1.openstream.co/981?aw_0_1st.collectionid%3D6906%26stationId%3D6906%26publisherId%3D1005%26k%3D1657900117%26aw_0_azn.pcountry%3D%5B%22US%22%5D%26aw_0_azn.planguage%3D%5B%22en%22%5D%26aw_0_azn.p'
+
+def exec(params):
+    audio_player.play(LOCATION)
+    return ''
