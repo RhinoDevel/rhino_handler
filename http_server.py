9612af664ebbf2006d7e788e2ee452ff9c3ac796
Marc <RhinoDevel@users.noreply.github.com>
2022-07-23 14:08:43 +0200

    Super simple HTTP server.

---
+++ http_server.py
@@ -0,0 +1,28 @@
+
+# Marcel Timm, RhinoDevel, 2022jul23
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+import responder_rhasspy
+
+ADDR = '127.0.0.1' # Do not use this non-locally (no security stuff at all).
+PORT = 7581
+
+class MtHttpHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_POST(self):
+        req_content_len = int(self.headers["Content-Length"])
+        req_body = self.rfile.read(req_content_len).decode() # 'utf-8'?
+
+        response_body = responder_rhasspy.exec_with_str(req_body)
+
+        self._set_headers()
+        self.wfile.write(response_body.encode('utf8'))
+
+httpd = HTTPServer((ADDR, PORT), MtHttpHandler)
+
+httpd.serve_forever()
