d6c367347453516ab2286c5c21d43b1a900e223a
Marc <RhinoDevel@users.noreply.github.com>
2022-07-23 12:48:03 +0200

    Move file stuff to responder module; Improve comments.

--- main.py
+++ main.py
@@ -6,71 +6,21 @@
 #
 # - Create a gzipped tarball from a folder:
 #
-#   tar cvzf TimmIntentHandler.tar.gz ./TimmIntentHandler
+#   tar cvzf rhino_handler.tar.gz ./rhino_handler
 #
 # - Execute BASH "inside" docker environment:
 #
 #   docker exec -it rhasspy bash
 
 import json
-import os
 import sys
 
 import responder
-
-# Will be saved to root folder inside docker container (is that OK?):
-#
-FILE_LAST_RESPONSE = 'last_response.txt'
-FILE_RUNS_LOCK     = 'rhino_handler.lock'
-
-def try_lock_running():
-    """Tries to set is-running-lock. Returns, if successful or not."""
-
-    try:
-        with open(FILE_RUNS_LOCK, 'x'):
-            return True
-    except:
-        return False # File seems to already exist.
-
-def unlock_running():
-    """Unlocks the is-running-lock."""
-
-    os.remove(FILE_RUNS_LOCK)
-
-def save_last_response(s):
-    """
-    Tries to save the given string to the last-response file. Nothing happens,
-    if saving fails.
-    """
-
-    try:
-        with open(FILE_LAST_RESPONSE, 'w') as f:
-            f.write(s)
-    except:
-        pass
-
-def load_last_response():
-    """Tries to load the last response from file. Returns None, if not found."""
- 
-    ret_val = None
-    l = None
-
-    try:
-        with open(FILE_LAST_RESPONSE) as f:
-            l = f.readlines()
-            if len(l) == 1:
-                ret_val = l[0]
-    except:
-        pass
-
-    return ret_val
 
 def get_params(obj):
     """Extract and return (intent) parameters from given object."""
 
     ret_val = {}
-
-    ret_val['last_response'] = load_last_response()
 
     # TODO: Implement adding parameters from input object!
 
@@ -79,16 +29,14 @@ def get_params(obj):
 def exec_with_obj(obj):
     """ 
     Get input from parameter (object) and augment that object with the 
-    response. Also saves that response as last response to a file.
+    response.
     """
 
     intent = obj['intent']['name']
     params = get_params(obj)
-    response = responder.exec(intent, params)
+    response = responder.exec(intent, params) # (augments parameters object)
 
     obj['speech'] = {'text': response} # Augments in-/output obj. with response.
-    
-    save_last_response(response)
 
 def exec_with_str(input_str):
     """Get input from parameter (string) and return response as string."""
@@ -110,16 +58,5 @@ def exec_with_std():
 
     print(output_str)
 
-def main():
-    if not try_lock_running():
-        return # No return value at all. Is this OK?
-
-    try:
-        exec_with_std()
-    except:
-        pass # Maybe no return value at all. Is this OK?
-
-    unlock_running()
-
 if __name__ == "__main__":
-    main()
+    exec_with_std()
--- responder.py
+++ responder.py
@@ -1,22 +1,100 @@
 
 # Marcel Timm, RhinoDevel, 2022jul17
 
+import os
 import importlib
 import importlib.util
 
 import intents
 
 INTENT_UNKNOWN = 'unknown' # Default for unknown intent.
+ERR_MSG_ON_EXCEPTION = 'Entschuldigung, es ist ein Ausnahmefehler aufgetreten.'
+
+FILE_LAST_RESPONSE = 'last_response.txt'
+FILE_RUNS_LOCK     = 'responder.lock'
+#
+# Will be saved to root folder inside docker container with Rhasspy 2.5.1 (is
+# that OK?).
+
+def try_lock_running():
+    """Tries to set is-running-lock. Returns, if successful or not."""
+
+    try:
+        with open(FILE_RUNS_LOCK, 'x'):
+            return True
+    except:
+        return False # File seems to already exist.
+
+def unlock_running():
+    """Unlocks the is-running-lock."""
+
+    os.remove(FILE_RUNS_LOCK)
+
+def save_last_response(s):
+    """
+    Tries to save the given string to the last-response file. Nothing happens,
+    if saving fails.
+    """
+
+    try:
+        with open(FILE_LAST_RESPONSE, 'w') as f:
+            f.write(s)
+    except:
+        pass
+
+def load_last_response():
+    """Tries to load the last response from file. Returns None, if not found."""
+ 
+    ret_val = None
+    l = None
+
+    try:
+        with open(FILE_LAST_RESPONSE) as f:
+            l = f.readlines()
+            if len(l) == 1:
+                ret_val = l[0]
+    except:
+        pass
+
+    return ret_val
 
 def get_module_path(intent):
     return intents.__name__ + '.' + intent.lower()
 
-def exec(intent, params):
-    """Get intent (name) and parameters (dictionary), return response string """
+def exec_without_lock(intent, params):
+    """
+    Get intent (name) and parameters (dictionary), return response string.
+    Augments given parameters object with last-response property.
+    Additionally saves response to last-response file.
+    """
 
+    ret_val = None
     module_path = get_module_path(intent)
 
     if(importlib.util.find_spec(module_path) is None):
         module_path = get_module_path(INTENT_UNKNOWN)
 
-    return importlib.import_module(module_path).exec(params)
+    # Augmenting parameters object with last-response property:
+    #
+    params['last_response'] = load_last_response()
+
+    ret_val = importlib.import_module(module_path).exec(params)
+
+    save_last_response(ret_val)
+
+    return ret_val
+
+def exec(intent, params):
+    ret_val = None
+
+    if not try_lock_running():
+        ret_val = '' # Returns an empty string, if lock cannot be acquired!
+    else:
+        try:
+            ret_val = exec_without_lock(intent, params)
+        except:
+            ret_val = ERR_MSG_ON_EXCEPTION
+
+        unlock_running()
+
+    return ret_val
