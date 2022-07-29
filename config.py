2fbd8e5f755f5ba19d829b1b56b526f5c310e36a
Marc <RhinoDevel@users.noreply.github.com>
2022-07-29 17:20:20 +0200

    Add Rhasspy address & port to config.

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ FILE_NAME = 'config.json'
 
 def _get_default():
     return {
-        'addr': 'pi4.fritz.box', # HTTP server's address.
-        'port': 7581             # HTTP server's port.
+        'addr': 'pi4.fritz.box',         # HTTP server's address.
+        'port': 7581,                    # HTTP server's port.
+        'rhasspy_addr': 'pi4.fritz.box', # Rhasspy server's address.
+        'rhasspy_port': 12101            # Rhasspy server's port.
     }
 
 def _try_load():
