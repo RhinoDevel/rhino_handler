a8cc1a0b9b8fcea1f3233887896b99dec354cfd0
Marc <RhinoDevel@users.noreply.github.com>
2022-07-29 17:21:49 +0200

    Play message on alert via Rhasspy server.

--- timer_exec.py
+++ timer_exec.py
@@ -2,15 +2,30 @@
 
 # Marcel Timm, RhinoDevel, 2022jul29
 
+import subprocess
+
+import config
 from timer import timer
 
 def _on_alert(msg):
     """Call this function for each timer that is due."""
 
-    print('ALERT: ' + '"' + msg + '"!') # TODO: Replace with something that makes sense!
+    c = config.get()
+    a = [
+            'curl',
+            '-X', 'POST',
+            'http://' + c['rhasspy_addr'] + ':' + str(c['rhasspy_port'])
+                + '/api/text-to-speech?play=true',
+            '-H', 'accept: audio/wav',
+            '-H', 'Content-Type: text/plain',
+            '-d', msg
+        ]
 
-    #curl -X POST "http://pi4.fritz.box:12101/api/text-to-speech?play=true" -H  "accept: audio/wav" -H  "Content-Type: text/plain" -d "Ding dong ding dong ding! Der Teimer ist abgelaufen."
-
+    subprocess.run(
+        a,
+        stdin = subprocess.DEVNULL,
+        stdout = subprocess.DEVNULL,
+        stderr = subprocess.DEVNULL)
 
 if __name__ == "__main__":
     timer.exec(_on_alert)
